b8675c6c8ece7db0fbb82906173c632fa3953f0a
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-09-14 23:34:08 -0400

    Fixed error #4

--- viz.js
+++ viz.js
@@ -133,6 +133,8 @@ $('.inc').click(function() {
                 if(startFlag !== false){
                     setTimeout(restart,1000);
                 }
+                d3.select("#wpm")
+                   .text(function(){return wpm();});  
                 });
 
 $('.dec').click(function() {
@@ -148,6 +150,8 @@ $('.dec').click(function() {
                 if(startFlag !== false){
                     setTimeout(restart,1000);
                 }
+                d3.select("#wpm")
+                   .text(function(){return wpm();});
                 });
 
 //pause on spacebar press
